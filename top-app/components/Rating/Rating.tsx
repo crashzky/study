eb514511e4c4c5b8cb080842958775e7d3b0556d
Kirill Osintsev <osintsev.2000@inbox.ru>
2021-10-30 18:32:47 +0300

    add tabIndex

--- top-app/components/Rating/Rating.tsx
+++ top-app/components/Rating/Rating.tsx
@@ -2,7 +2,7 @@ import Props from './Rating.props';
 import styles from './Rating.module.scss';
 import StarIcon from './star.svg';
 import cn from 'classnames';
-import { useState } from 'react';
+import { useState, KeyboardEvent } from 'react';
 
 export const Rating = ({ activedStars = [0, 0, 0, 0, 0], setActivedStar, ...props }: Props): JSX.Element => {
 	const [hoveredStars, setHoveredStars] = useState<number>(-1);
@@ -17,12 +17,17 @@ export const Rating = ({ activedStars = [0, 0, 0, 0, 0], setActivedStar, ...prop
 					key={num}
 					className={styles.button}
 					onClick={() => setActivedStar(num)}
-					onMouseEnter={() => setHoveredStars((num))}
+					onKeyDown={(e: KeyboardEvent<HTMLButtonElement>) => {
+						if(e.code === 'Space')
+							setHoveredStars(num);
+					}}
+					onMouseEnter={() => setHoveredStars(num)}
 					onMouseLeave={() => setHoveredStars(num - 1)}
 				>
 					<StarIcon
 						tabIndex={0}
 						className={cn(
+							styles.star,
 							{
 								[styles.active]: (i === 1 && hoveredStars === -1) || (hoveredStars !== -1 && num <= hoveredStars),
 							}
